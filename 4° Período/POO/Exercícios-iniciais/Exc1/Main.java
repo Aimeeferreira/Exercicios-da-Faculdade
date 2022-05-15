2bab54a9d434b9c6faf0e6ea838d666f2708c53f
Aimeeferreira <Aimeeferreira19@gmail.com>
2022-05-15 19:08:03 -0300

    confirmar em main

---
+++ 4° Período/POO/Exercícios-iniciais/Exc1/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in); 
+        Trabalhador TrabalhadorA = new Trabalhador();
+        Trabalhador TrabalhadorB = new Trabalhador();
+        Trabalhador TrabalhadorC = new Trabalhador();
+
+        System.out.println("Nome: ");
+        TrabalhadorA.setNome(input.nextLine());
+        System.out.println("Matricula: ");
+        TrabalhadorA.setMatricula(input.nextInt());
+        System.out.println("Salario: ");
+        TrabalhadorA.setSalario(input.nextDouble());
+        if(TrabalhadorA.getSalario() < 1100.00){
+            TrabalhadorA.setSalario(1100.00);
+        }
+
+        System.out.println("\nNome: ");
+        TrabalhadorB.setNome(input.next());
+        System.out.println("Matricula: ");
+        TrabalhadorB.setMatricula(input.nextInt());
+        System.out.println("Salario: ");
+        TrabalhadorB.setSalario(input.nextDouble());
+        if(TrabalhadorB.getSalario() < 1100.00){
+            TrabalhadorB.setSalario(1100.00);
+        }
+
+        System.out.println("\nNome: ");
+        TrabalhadorC.setNome(input.next());
+        System.out.println("Matricula: ");
+        TrabalhadorC.setMatricula(input.nextInt());
+        System.out.println("Salario: ");
+        TrabalhadorC.setSalario(input.nextDouble());
+        if(TrabalhadorC.getSalario() < 1100.00){
+            TrabalhadorC.setSalario(1100.00);
+        }
+
+        System.out.println("Nome: "+TrabalhadorA.getNome() +"  Matricula: " + TrabalhadorA.getMatricula() +"  Salario: " + TrabalhadorA.getSalario());
+        System.out.println("Nome: "+TrabalhadorB.getNome() +"  Matricula: " + TrabalhadorB.getMatricula() +"  Salario: " + TrabalhadorB.getSalario());
+        System.out.println("Nome: "+TrabalhadorC.getNome() +"  Matricula: " + TrabalhadorC.getMatricula() +"  Salario: " + TrabalhadorC.getSalario());
+
+        input.close();
+    }
+}
---
+++ 4° Período/POO/Exercícios-iniciais/Exc2/Eleitor.java
@@ -0,0 +1,5 @@
+package 4° Período.POO.Exercícios-iniciais.Exc2;
+
+public class Eleitor {
+    
+}
